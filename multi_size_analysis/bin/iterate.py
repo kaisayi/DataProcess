5f9fe951d05ac7d9aee8b4a7fff15843915ab4a0
kaisayi <1209086740@qq.com>
2017-11-16 10:26:24 +0800

    edit the iterate file

--- multi_size_analysis/bin/iterate.py
+++ multi_size_analysis/bin/iterate.py
@@ -7,6 +7,9 @@
 
 from preprocess import  *
 from postprocess import *
+import numpy as np
+
+_submit_inp_commands = 'abaqus job=../'
 
 class iteration:
 
@@ -14,5 +17,13 @@ class iteration:
         self.initvalue = [3.45234, 0.55473, 28.2832]
         self.step = 0.1
         self.error = 1e-2
+        self.delta = [0.1, 0.05, 0.5]
 
-    def
+    def grediant(self):
+
+        # modify the original inp file
+        modify(*self.initvalue)
+
+        # submit the inp file into abaqus to calculate the simulation result
+
+
